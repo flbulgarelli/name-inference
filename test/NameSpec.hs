70c91bacb897465591298b5b9aeb7be76714864c
Franco Bulgarelli <franco@mumuki.org>
2019-11-10 13:46:02 -0300

    Adding another test

--- test/NameSpec.hs
+++ test/NameSpec.hs
@@ -3,12 +3,13 @@ module NameSpec (spec) where
 import  Test.Hspec
 import  Data.Name
 
-sampleBag = Bag ["Daniela", "Rocío", "Federico", "Juan", "Florencia", "Ana", "Gonzalo", "Rodrigo", "Franco", "Washignton"] ["Scarpa", "Villani", "Tata", "Rodrigo", "Gonzalo", "Finzi", "Rodríguez", "Washignton"]
+sampleBag = Bag ["Daniela", "Rocío", "Federico", "Juan", "María", "Beatriz", "Florencia", "Ana", "Gonzalo", "Rodrigo", "Franco", "Washignton"] ["Scarpa", "Villani", "Tata", "Rodrigo", "Gonzalo", "Finzi", "Rodríguez", "Washignton"]
 run = fix sampleBag
 
 spec :: Spec
 spec = do
   describe "fix" $ do
+
     it "N S" $ run (NameAndSurname ["Federico"] ["Scarpa"]) `shouldBe` (FixedName (Sure ["Federico"]) (Sure ["Scarpa"]))
     it "S N" $ run (NameAndSurname ["Villani"] ["Daniela"]) `shouldBe` (FixedName (Sure ["Daniela"]) (Sure ["Villani"]))
     it "O O" $ run (NameAndSurname ["Rodrigo"] ["Alfonso"]) `shouldBe` (FixedName (Unsure ["Rodrigo"]) (Unsure ["Alfonso"]))
@@ -28,6 +29,17 @@ spec = do
     it "OS N"  $ run (FullName ["Florencia", "Tata", "Finzi"]) `shouldBe` (FixedName (Sure ["Florencia"]) (Sure ["Tata", "Finzi"]))
     it "OS N"  $ run (FullName ["Rodrigo", "Juan", "Alfonso"]) `shouldBe` (FixedName (Sure ["Rodrigo", "Juan"]) (Unsure ["Alfonso"]))
 
+    it "S S" $ run (NameAndSurname ["Scarpa"] ["Tata"]) `shouldBe` (FixedName (Unsure ["Scarpa"]) (Unsure ["Tata"]))
+
     it "NOOSOO"  $ run (FullName ["Ana", "María", "Beatriz", "Rodríguez", "Alfonso", "De la Cruz"]) `shouldBe` (
                                                                                                         FixedName (Sure ["Ana", "María", "Beatriz"]) (Sure ["Rodríguez", "Alfonso", "De la Cruz"]))
 
+
+  describe "fixes" $ do
+    it "" $  do
+      fixes sampleBag (FullName ["Ana", "María", "Beatriz", "Rodríguez", "Alfonso", "De la Cruz"]) `shouldBe` [
+        FixedName (Sure ["Ana"]) (Unsure ["María","Beatriz","Rodríguez","Alfonso","De la Cruz"]),
+        FixedName (Sure ["Ana","María"]) (Unsure ["Beatriz","Rodríguez","Alfonso","De la Cruz"]),
+        FixedName (Sure ["Ana","María","Beatriz"]) (Sure ["Rodríguez","Alfonso","De la Cruz"]),
+        FixedName (Unsure ["Ana","María","Beatriz","Rodríguez"]) (Unsure ["Alfonso","De la Cruz"]),
+        FixedName (Unsure ["Ana","María","Beatriz","Rodríguez","Alfonso"]) (Unsure ["De la Cruz"])]
