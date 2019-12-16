69aebdddd9fee1e69f84ab89dc6fa30d77d9494b
Franco Bulgarelli <franco@mumuki.org>
2019-12-16 18:36:21 -0300

    Adding integration tests

---
+++ test/IntegrationSpec.hs
@@ -0,0 +1,53 @@
+module IntegrationSpec (spec) where
+
+import  Test.Hspec
+import  Data.Name
+import System.IO.Unsafe
+
+sampleRegistry = unsafePerformIO $ do
+  names <- fmap lines (readFile "test/data/givens.txt")
+  surnames <- fmap lines (readFile "test/data/families.txt")
+  return $ (makeRegistry names surnames) { treatUnknownAsFamily = True }
+
+run = fix sampleRegistry
+runMaybe = fixMaybe sampleRegistry
+
+spec :: Spec
+spec = do
+  describe "fix" $ do
+    describe "GivenAndFamily" $ do
+
+      it "N S" $ run (GivenAndFamily ["Rocío"] ["Gonzalez"]) `shouldBe` (GivenAndFamily ["Rocío"] ["Gonzalez"])
+      it "S N" $ run (GivenAndFamily ["Calvo"] ["Felipe"]) `shouldBe` (GivenAndFamily ["Felipe"] ["Calvo"])
+
+      it "NN S" $ run (GivenAndFamily ["Federico", "Alfredo"] ["Scarpa"]) `shouldBe` (GivenAndFamily ["Federico", "Alfredo"] ["Scarpa"])
+      it "S NN" $ run (GivenAndFamily ["Scarpa"] ["Federico", "Alfredo"]) `shouldBe` (GivenAndFamily ["Federico", "Alfredo"] ["Scarpa"])
+
+      it "N SS" $ run (GivenAndFamily ["Ivana"] ["Feldfeber", "Kivelsky"]) `shouldBe` (GivenAndFamily ["Ivana"] ["Feldfeber", "Kivelsky"])
+      it "SS N" $ run (GivenAndFamily ["Feldfeber", "Kivelsky"] ["Ivana"]) `shouldBe` (GivenAndFamily ["Ivana"] ["Feldfeber", "Kivelsky"])
+
+      it "S S" $ runMaybe (GivenAndFamily ["Bulgarelli"] ["Alt"]) `shouldBe` Nothing
+
+      it "N N" $ runMaybe (GivenAndFamily ["Laura"] ["Giselle"]) `shouldBe` Nothing
+
+      it "SS A" $ run (GivenAndFamily ["Villani", "Trucco"] ["Alfonso"]) `shouldBe` GivenAndFamily ["Alfonso"] ["Villani", "Trucco"]
+
+
+    describe "FullName" $ do
+
+      it "NS" $ run (FullName ["Rocío", "Gonzalez"]) `shouldBe` (GivenAndFamily ["Rocío"] ["Gonzalez"])
+      it "SN" $ run (FullName ["Calvo", "Felipe"]) `shouldBe` (GivenAndFamily ["Felipe"] ["Calvo"])
+
+      it "NNS" $ run (FullName ["Federico", "Alfredo", "Scarpa"]) `shouldBe` (GivenAndFamily ["Federico", "Alfredo"] ["Scarpa"])
+      it "SNN" $ run (FullName ["Scarpa", "Federico", "Alfredo"]) `shouldBe` (GivenAndFamily ["Federico", "Alfredo"] ["Scarpa"])
+
+      it "NSS" $ run (FullName ["Ivana", "Feldfeber", "Kivelsky"]) `shouldBe` (GivenAndFamily ["Ivana"] ["Feldfeber", "Kivelsky"])
+      it "SSN" $ run (FullName ["Feldfeber", "Kivelsky", "Ivana"]) `shouldBe` (GivenAndFamily ["Ivana"] ["Feldfeber", "Kivelsky"])
+
+      it "SS" $ runMaybe (FullName ["Bulgarelli", "Alt"]) `shouldBe` Nothing
+
+      it "NN" $ runMaybe (FullName ["Laura", "Giselle"]) `shouldBe` Nothing
+
+      it "SSA" $ run (FullName ["Villani", "Trucco", "Alfonso"]) `shouldBe` GivenAndFamily ["Alfonso"] ["Villani", "Trucco"]
+
+
