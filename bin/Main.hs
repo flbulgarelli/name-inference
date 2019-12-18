20409e147ef492a0eea614cc0bdeada59c22bd9f
Franco Bulgarelli <franco@mumuki.org>
2019-12-17 21:56:15 -0300

    Adding output formats

--- bin/Main.hs
+++ bin/Main.hs
@@ -6,12 +6,14 @@ import Options.Applicative
 import Data.Semigroup ((<>))
 import Data.Foldable (for_)
 import Data.Text as T (Text, pack, unpack, splitOn, strip, replace, toTitle, null)
+import Data.List (intercalate)
 import Data.PersonalName
 
 data Input = Input {
     givensFile :: String,
     familiesFile :: String,
     inputFile :: String,
+    outputFormat :: String,
     transliterate :: Bool,
     unknownAsFamily :: Bool
   }
@@ -34,6 +36,12 @@ sample = Input
          <> metavar "FILE"
          <> value "--"
          <> help "families filename" )
+      <*> strOption (
+        long "output-format"
+         <> short 'X'
+         <> metavar "tagged|csv|padded"
+         <> value "tagged"
+         <> help "output format. `tagged` by default" )
       <*> switch (
         long "transliterate"
          <> short 't'
@@ -49,15 +57,34 @@ main = execParser opts >>= run
     opts = info (sample <**> helper) (fullDesc <> progDesc "Classify and flip personal names")
 
 run :: Input -> IO ()
-run (Input givens families file transliterate unknownAsFamily) = do
+run (Input givens families file outputFormat transliterate unknownAsFamily) = do
   givens <- fmap lines (readFile givens)
   families <- fmap lines (readFile families)
   let registry = makeRegistry givens families (RegistryOptions transliterate unknownAsFamily)
   contents <- if file == "--" then getContents else readFile file
-  for_ (lines contents) (processLine registry)
+  for_ (lines contents) (processLine registry $ selectFormat outputFormat)
 
-processLine :: Registry -> String -> IO ()
-processLine registry = putStrLn  . show . fix registry . FullName . map unpack . prepare . pack
+processLine :: Registry -> Format -> String -> IO ()
+processLine registry format = putStrLn  . format . fix registry . FullName . map unpack . prepare . pack
+
+type Format = PersonalName -> String
+
+selectFormat :: String -> Format
+selectFormat "tagged" = tagged
+selectFormat "csv"    = csv
+selectFormat _        = padded
+
+tagged :: Format
+tagged (FullName names)       = "FullName:" ++ intercalate " " names
+tagged (GivenAndFamily gs fs) = "GivenAndFamily:" ++ intercalate " " gs ++ "," ++ intercalate " " fs
+
+csv :: Format
+csv (FullName names)       = intercalate " " names
+csv (GivenAndFamily gs fs) = intercalate " " gs ++ "," ++ intercalate " " fs
+
+padded :: Format
+padded (FullName names)       = ",," ++ intercalate " " names
+padded (GivenAndFamily gs fs) = intercalate " " gs ++ "," ++ intercalate " " fs
 
 prepare :: Text -> [Text]
 prepare = filter (not . T.null) . map (toTitle . strip) . splitOn " " . replace "," " "
