c7ed62cf01738b2126188fef8905f80ab01f4eb8
Franco Bulgarelli <franco@mumuki.org>
2019-12-17 21:11:35 -0300

    Allowing to read from file and prepare lines

--- bin/Main.hs
+++ bin/Main.hs
@@ -1,27 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
 import Options.Applicative
 import Data.Semigroup ((<>))
 import Data.Foldable (for_)
-import Data.List.Split (splitOn)
+import Data.Text as T (Text, pack, unpack, splitOn, strip, replace, toTitle, null)
 import Data.PersonalName
 
 data Input = Input {
     givensFile :: String,
-    familiesFile :: String
+    familiesFile :: String,
+    inputFile :: String,
+    transliterate :: Bool,
+    unknownAsFamily :: Bool
   }
 
 sample :: Parser Input
 sample = Input
       <$> strOption (
         long "givens"
+         <> short 'g'
          <> metavar "FILE"
          <> help "givens filename" )
       <*> strOption (
         long "families"
+         <> short 'f'
          <> metavar "FILE"
          <> help "families filename" )
-
+      <*> strOption (
+        long "file"
+         <> short 'F'
+         <> metavar "FILE"
+         <> value "--"
+         <> help "families filename" )
+      <*> switch (
+        long "transliterate"
+         <> short 't'
+         <> help "transliterate names" )
+      <*> switch (
+        long "unknown-as-family"
+         <> short 'u'
+         <> help "Treat unknown names as family names")
 
 main :: IO ()
 main = execParser opts >>= run
@@ -29,14 +49,16 @@ main = execParser opts >>= run
     opts = info (sample <**> helper) (fullDesc <> progDesc "Classify and flip personal names")
 
 run :: Input -> IO ()
-run (Input givens families) = do
+run (Input givens families file transliterate unknownAsFamily) = do
   givens <- fmap lines (readFile givens)
   families <- fmap lines (readFile families)
-  let registry = makeRegistry givens families (defaultOptions { transliterateNames = True, treatUnknownAsFamily = True })
-  contents <- getContents
+  let registry = makeRegistry givens families (RegistryOptions transliterate unknownAsFamily)
+  contents <- if file == "--" then getContents else readFile file
   for_ (lines contents) (processLine registry)
 
 processLine :: Registry -> String -> IO ()
-processLine registry = putStrLn  . show . fix registry . FullName . splitOn " "
+processLine registry = putStrLn  . show . fix registry . FullName . map unpack . prepare . pack
 
+prepare :: Text -> [Text]
+prepare = filter (not . T.null) . map (toTitle . strip) . splitOn " " . replace "," " "
 
